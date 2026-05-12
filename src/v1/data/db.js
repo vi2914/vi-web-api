0b5a3a0206c39020759d2b2261aca26b18f1b9c5
snik109 <jonas.flydahl@gmail.com>
2026-05-12 14:15:40 +0200

    effectively removed the connection limit to fix an issue with the frontend

--- src/v1/data/db.js
+++ src/v1/data/db.js
@@ -34,7 +34,7 @@ export async function getPool() {
             database,
             password: DB_PASSWORD,
             waitForConnections: true,
-            connectionLimit: 10,
+            connectionLimit: 10000000,
             queueLimit: 0
         });
     } catch (error) {
